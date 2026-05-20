68b52f18c7bbc5908b1778ce1f6b125dccac7a17
Rifkychandra93 <ryusukagarden@gmail.com>
2026-05-20 09:41:01 +0700

    perbaikan

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const api = axios.create({
-  baseURL: "http://192.168.0.104:3000/api",
+  baseURL: "http://[IP_ADDRESS]/api",
 });
 
 api.interceptors.request.use(async (config) => {
